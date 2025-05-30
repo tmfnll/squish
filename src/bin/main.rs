277a33690dd5a154b4a6c63997b37f788513dc3a
Tom Finill <tom.finill@ontruck.com>
2025-05-31 00:57:41 +0200

    Make an LED blink instead of just doing nothing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,30 +1,29 @@
+//! Blinks an LED
+//!
+//! This assumes that a LED is connected to the pin assigned to `led`. (GPIO4)
+
 #![no_std]
 #![no_main]
 
-use esp_hal::clock::CpuClock;
-use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
-
-#[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
-
-extern crate alloc;
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Level, Output, OutputConfig},
+    main
+};
+use log::info;
 
 #[main]
 fn main() -> ! {
-    // generator version: 0.3.1
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    esp_println::logger::init_logger_from_env();
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
-
-    esp_alloc::heap_allocator!(size: 72 * 1024);
+    let mut led = Output::new(peripherals.GPIO4, Level::High, OutputConfig::default());
+    let delay = Delay::new();
 
     loop {
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        info!("Toggling LED...");
+        led.toggle();
+        delay.delay_millis(500);
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
 }
