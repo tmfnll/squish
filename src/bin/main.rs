c2f506f034ba76528d2628f2514f278114aba8fb
Tom Finill <tom.finill@ontruck.com>
2025-06-02 22:19:46 +0200

    Add a linear potentiometer and perform basic monitoring

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,21 +9,41 @@ use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
     gpio::{Level, Output, OutputConfig},
-    main
+    main,
 };
 use log::info;
+use squish::LinearPotentiometer;
 
 #[main]
 fn main() -> ! {
     let peripherals = esp_hal::init(esp_hal::Config::default());
+
     esp_println::logger::init_logger_from_env();
 
     let mut led = Output::new(peripherals.GPIO4, Level::High, OutputConfig::default());
+
+    let mut potentiometer = LinearPotentiometer::new(peripherals.GPIO5, peripherals.ADC1);
+
     let delay = Delay::new();
 
+    let mut pin_value: u16;
+    let mut old_pin_value: u16 = 1;
+
     loop {
-        info!("Toggling LED...");
-        led.toggle();
-        delay.delay_millis(500);
+        delay.delay_millis(1);
+
+        pin_value = potentiometer.read();
+
+        if pin_value > 3000 {
+            led.set_high();
+        } else {
+            led.set_low();
+        }
+
+        if pin_value != old_pin_value {
+            info!("Potentiometer value: {}", pin_value);
+        }
+
+        old_pin_value = pin_value;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
 #![no_std]
+
+use esp_hal::analog::adc::RegisterAccess;
+use esp_hal::analog::adc::{Adc, AdcChannel, AdcConfig, AdcPin, Attenuation};
+use esp_hal::gpio::AnalogPin;
+use esp_hal::peripheral::Peripheral;
+use esp_hal::Blocking;
+use nb::block;
+
+pub struct LinearPotentiometer<'a, ADCI, PIN> {
+    adc_pin: AdcPin<PIN, ADCI>,
+    adc: Adc<'a, ADCI, Blocking>,
+}
+
+impl<'a, ADCI, PIN> LinearPotentiometer<'a, ADCI, PIN>
+where
+    PIN: AdcChannel + AnalogPin,
+    ADCI: RegisterAccess + 'a,
+{
+    pub fn new(analog_pin: PIN, adc_instance: impl Peripheral<P = ADCI> + 'a) -> Self {
+        let mut adc_config = AdcConfig::new();
+
+        let adc_pin = adc_config.enable_pin(analog_pin, Attenuation::_11dB);
+
+        let adc = Adc::new(adc_instance, adc_config);
+
+        Self { adc_pin, adc }
+    }
+
+    pub fn read(&mut self) -> u16 {
+        block!(self.adc.read_oneshot(&mut self.adc_pin)).unwrap()
+    }
+}
